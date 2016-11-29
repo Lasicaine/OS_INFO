01ee1022b3a6a528ef539364b5b947fa018a36ad
Suomaa <a.antomonov@mail.ru>
2016-11-29 23:59:16 +0300

    Add OS info and up version used package

--- js/script.js
+++ js/script.js
@@ -11,9 +11,14 @@ $(function(){
     const os = require('os');
     var prettyBytes = require('pretty-bytes');
 
-    $('.stats').append('Node: <span>' + versions.node + '</span>');
-    $('.stats').append('Electron: <span>' + versions.electron + '</span>');
-    $('.stats').append('Chrome: <span>' + versions.chrome + '</span>');
+    $('.stats').append('Node version: <span>' + versions.node + '</span>');
+    $('.stats').append('Electron version: <span>' + versions.electron + '</span>');
+    $('.stats').append('Chrome version: <span>' + versions.chrome + '</span>');
+
+    $('.stats').append('OS type: <span>' + os.type() + '</span>');
+    $('.stats').append('OS platform: <span>' + os.platform() + '</span>');
+    $('.stats').append('OS release: <span>' + os.release() + '</span>');
+
     $('.stats').append('CPU architecture: <span>' + os.arch() + '</span>');
     $('.stats').append('CPU Model: <span>' + os.cpus()[0].model + '</span>');
     $('.stats').append('CPU Speed: <span>' + os.cpus()[0].speed + ' MHz ' + '</span>');
@@ -23,6 +28,11 @@ $(function(){
     $('.stats').append('Total memory: <span>' + prettyBytes(os.totalmem())+ '</span>');
     $('.stats').append('Free memory: <span>' + prettyBytes(os.freemem())+ '</span>');
 
-    $('.stats').append('Home Directory: <span>' + os.homedir()+ '</span>');
+    $('.stats').append('Home directory: <span>' + os.homedir()+ '</span>');
     $('.stats').append('Hostname: <span>' + os.hostname() + '</span>');
+    $('.stats').append('Temporary directory: <span>' + os.tmpdir() + '</span>');
+
+    $('.stats').append('OS uptime: <span>' + os.uptime() + '</span>');
+    $('.stats').append('Load Averages: <span>' + os.loadavg()[0] + ', ' + os.loadavg()[1] + ', ' + os.loadavg()[2] + '</span>');
+
 });
